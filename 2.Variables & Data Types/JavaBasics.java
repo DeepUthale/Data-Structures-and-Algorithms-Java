dd19170d609c45895142fd8626aa76d5af45a76e
DeepUthale <113908422+DeepUthale@users.noreply.github.com>
2025-04-14 01:22:55 +0530

    Delete 2.Variables & Data Types directory

--- 2.Variables & Data Types/JavaBasics.java
+++
@@ -1,8 +0,0 @@
-public class JavaBasics {
-    public static void main(String[] args) {
-        System.out.print("Hello World! \n"); // line space (ln, \n)
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-    }
-}
-// boilerplate code
